93c07cde17817fd0b9168b20700c1df67c72374a
carolina <carolina.acosta@thoughtworks.com>
2022-10-26 14:51:41 +0200

    update pipeline

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs')
 
 try {
-    // throw(new Error("some error message"));
     const name = core.getInput('who-to-greet')
-    console.log(`Hello ${name}`)
 
-    const time = new Date();
-    core.setOutput("time", time.toTimeString());
+    function buildTree(startPath){
+        fs.readdir(startPath, (err, entries) => {
+            console.log(entries);
+        });
+    }
 
-    console.log(JSON.stringify(github, null, '\t'))
+    buildTree('/Users/carolina.acosta/Proyectos')
+    // throw(new Error("some error message"));
+    // const name = core.getInput('who-to-greet')
+    // console.log(`Hello ${name}`)
+
+    // const time = new Date();
+    // core.setOutput("time", time.toTimeString());
+
+    // console.log(JSON.stringify(github, null, '\t'))
 } catch (error) {
     core.setFailed(error.message)
 }
